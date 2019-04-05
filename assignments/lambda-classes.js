df0f6c04ccf3f6bb09b7d08454ad43bf667b15e9
Robert Pedersen <rob84044@users.noreply.github.com>
2019-04-04 20:52:00 -0600

    lambda-classes.js
    Finished the Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+
+class Person{
+  constructor(values){
+    this.name = values.name;
+    this.age = values.age;
+    this.location = values.location;
+    this.gender = values.gender;
+    this.name = values.name;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
+}
+
+class Instructor extends Person{
+  constructor (){
+    
+  }
+}
+
